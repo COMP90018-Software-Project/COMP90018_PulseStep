b667e44be1883dc6518d5a046b9bba756712b0f0
yuk1-zhang <3076434698@qq.com>
2024-09-26 21:41:57 +0800

    add navigation bar

--- PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/MainActivity.java
+++ PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.pulsestepapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.pulsestepapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new WorkoutFragment());
+        Log.d("DEBUG", "home page: workout fragment");
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+                switch(item.getItemId()){
+
+                    case R.id.workout:
+                        replaceFragment(new WorkoutFragment());
+                        Log.d("DEBUG", "change to workout");
+                        break;
+                    case R.id.ranking:
+                        replaceFragment(new RankingFragment());
+                        Log.d("DEBUG", "change to ranking");
+                        break;
+                    case R.id.profile:
+                        replaceFragment(new ProfileFragment());
+                        Log.d("DEBUG", "change to profile");
+                        break;
+                }
+
+                return true;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
