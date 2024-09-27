e125ae60c09045ed7c9934bff00567f41ca72892
susyyyy-y <yeshiwenh1@gmail.com>
2024-09-27 23:37:43 +1000

    Create Jump Summary Page

---
+++ PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/JumpSummaryActivity.java
@@ -0,0 +1,33 @@
+package com.example.pulsestepapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JumpSummaryActivity extends AppCompatActivity {
+
+    private Button finishButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_summary);
+
+        // Reference to the Finish button
+        finishButton = findViewById(R.id.bt_finish);
+
+        // Handle Finish button click
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the activity and return to the previous screen
+                Intent intent = new Intent(JumpSummaryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
