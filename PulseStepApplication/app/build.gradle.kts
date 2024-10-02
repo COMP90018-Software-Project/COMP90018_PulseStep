8976ff966b4a0fc6c3fb99515d61fc454019a67c
Jackie Cheng <pccheng1@student.unimelb.edu.au>
2024-10-03 01:45:59 +1000

    merge gradle and xml

--- PulseStepApplication/app/build.gradle.kts
+++ PulseStepApplication/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -12,6 +13,9 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+        }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -32,9 +36,16 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("libs")
+        }
+    }
 }
 
 dependencies {
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("com.github.dhaval2404:imagepicker:2.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation("de.hdodenhof:circleimageview:3.0.0")
