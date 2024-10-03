b439fde0b08923fc559288332c39656c82ee908f
Jackie Cheng <pccheng1@student.unimelb.edu.au>
2024-10-03 10:29:38 +1000

    do not fresh page in every click

--- PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/MainActivity.java
+++ PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/MainActivity.java
@@ -21,39 +21,69 @@ import com.example.pulsestepapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+
+    // Cache the WorkoutFragment instance
+    private Fragment workoutFragment;
+    private Fragment activeFragment; // The currently displayed Fragment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        replaceFragment(new WorkoutFragment());
+        // Initialize and display WorkoutFragment
+        workoutFragment = new WorkoutFragment();
+        activeFragment = workoutFragment; // Set the initial fragment to workoutFragment
+
+        // Use add() to add the initial Fragment and display it
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.frameLayout, workoutFragment, "workout")
+                .commit();
+
         Log.d("DEBUG", "home page: workout fragment");
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-                switch(item.getItemId()){
-                    case R.id.workout:
-                        replaceFragment(new WorkoutFragment());
-                        Log.d("DEBUG", "change to workout");
-                        break;
-                    case R.id.ranking:
-                        replaceFragment(new RankingFragment());
-                        Log.d("DEBUG", "change to ranking");
-                        break;
-                    case R.id.profile:
-                        replaceFragment(new ProfileFragment());
-                        Log.d("DEBUG", "change to profile");
-                        break;
-                }
-
-                return true;
+            switch (item.getItemId()) {
+                case R.id.workout:
+                    showFragment(workoutFragment);
+                    Log.d("DEBUG", "change to workout");
+                    break;
+                case R.id.ranking:
+                    showFragment(new RankingFragment());
+                    Log.d("DEBUG", "change to ranking");
+                    break;
+                case R.id.profile:
+                    showFragment(new ProfileFragment());
+                    Log.d("DEBUG", "change to profile");
+                    break;
+            }
+            return true;
         });
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, fragment);
-        fragmentTransaction.commit();
+    /**
+     * Displays the specified Fragment using show() and hide() to avoid refreshing.
+     */
+    private void showFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // If the fragment to show is the cached workoutFragment
+        if (fragment == workoutFragment) {
+            // Only show and hide the other Fragment
+            transaction.hide(activeFragment).show(workoutFragment);
+        } else {
+            // If it is not the workoutFragment
+            if (!fragment.isAdded()) {
+                // If the fragment is not added, add it and hide the current Fragment
+                transaction.hide(activeFragment).add(R.id.frameLayout, fragment);
+            } else {
+                // If the fragment is already added, directly show it and hide the current Fragment
+                transaction.hide(activeFragment).show(fragment);
+            }
+        }
+
+        transaction.commit();
+        activeFragment = fragment; // Update the currently displayed Fragment
     }
 }
