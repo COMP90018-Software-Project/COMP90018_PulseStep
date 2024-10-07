7dd3e9dc6e2c33b96afe3a92690589cc8b454620
susyyyy-y <yeshiwenh1@gmail.com>
2024-10-07 15:16:33 +1100

    change commit

--- PulseStepApplication/app/build.gradle.kts
+++ PulseStepApplication/app/build.gradle.kts
@@ -9,16 +9,18 @@ android {
 
     defaultConfig {
         applicationId = "com.example.pulsestepapplication"
-        minSdk = 33
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         ndk {
-            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+            abiFilters.addAll(listOf("x86_64", "x86","armeabi-v7a", "arm64-v8a"))
         }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+
 
     buildTypes {
         release {
