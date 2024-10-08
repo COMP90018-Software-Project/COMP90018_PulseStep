2059beda5721709bd07fd1d9a620be913dd70305
yuk1-zhang <3076434698@qq.com>
2024-10-08 15:06:10 +0800

    a

--- PulseStepApplication/app/build.gradle.kts
+++ PulseStepApplication/app/build.gradle.kts
@@ -64,4 +64,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
