255c246cd9215073cad3f9a810bb72cd971c9418
susyyyy-y <yeshiwenh1@gmail.com>
2024-10-10 20:26:09 +1100

    merge

--- PulseStepApplication/app/build.gradle.kts
+++ PulseStepApplication/app/build.gradle.kts
@@ -15,9 +15,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        ndk {
-            abiFilters.addAll(listOf("x86_64", "x86","armeabi-v7a", "arm64-v8a"))//
-        }
+
 //
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
