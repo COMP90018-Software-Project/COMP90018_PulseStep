66e2a2b9080b66a480f1fcbeafa457d2f3201873
susyyyy-y <yeshiwenh1@gmail.com>
2024-10-10 17:22:22 +1100

    Modified the color of UI: uniform dark and light mode

--- PulseStepApplication/app/build.gradle.kts
+++ PulseStepApplication/app/build.gradle.kts
@@ -16,9 +16,9 @@ android {
         versionCode = 1
         versionName = "1.0"
         ndk {
-            abiFilters.addAll(listOf("x86_64", "x86"))//,"armeabi-v7a", "arm64-v8a"
+            abiFilters.addAll(listOf("x86_64", "x86","armeabi-v7a", "arm64-v8a"))//
         }
-
+//
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
