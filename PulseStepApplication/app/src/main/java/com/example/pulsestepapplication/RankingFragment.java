7d6d91e37eb5898bfffcc62cf595a701f492a998
susyyyy-y <yeshiwenh1@gmail.com>
2024-10-12 03:26:00 +1100

    Set real time user monthly and daily total activity time row

--- PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/RankingFragment.java
+++ PulseStepApplication/app/src/main/java/com/example/pulsestepapplication/RankingFragment.java
@@ -48,22 +48,28 @@ public class RankingFragment extends Fragment {
 
     private TextView dailyTargetTextView;
     private TextView dailyActiveTimeTextView;
+    private TextView topNameTextView;
+    private TextView topActiveTimeTextView;
     private String targetHours;
     private String dailyActiveTime;
+    private String monthlyActiveTime;
+    private String userTotalActiveTime;
     private FirebaseFirestore db;
     private FirebaseUser currentUser;
     private String userId;
+    private String userName;
     private ProgressBar progressBar;
+    private boolean isMonthlyRank;
 
     public RankingFragment() {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setUpRankModels();
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setUpRankModels();
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -84,6 +90,10 @@ public class RankingFragment extends Fragment {
         dailyTargetTextView = view.findViewById(R.id.daily_target_hours);
         dailyActiveTimeTextView = view.findViewById(R.id.progress_detail);
 
+        // View of the row above rank list (you daily or monthly active time)
+        topNameTextView = view.findViewById(R.id.you_name);
+        topActiveTimeTextView = view.findViewById(R.id.you_time);
+
         // View of progress bar
         progressBar = view.findViewById(R.id.progressBar);
 
@@ -94,12 +104,8 @@ public class RankingFragment extends Fragment {
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         userId = currentUser.getUid();
 
-        // 从 Firebase 获取当前用户的 target 并更新 UI
-        fetchTargetData();
-//        fetchDailyActiveHours();
-
         ImageView editTarget = view.findViewById(R.id.bt_set_target);
-        // 给 dailyTargetTextView 设置点击事件，触发 Dialog
+        // setTarget Dialog
         editTarget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,16 +124,25 @@ public class RankingFragment extends Fragment {
                     switch (checkedId) {
                         case R.id.bt_daily:
                             titleTextView.setText("Daily Sports Rankings");
+                            isMonthlyRank = false;
 //                            updateRecyclerViewData("daily");
+                            // Fetch target and progress data from firestore
+                            fetchTargetData();
                             break;
                         case R.id.bt_monthly:
                             titleTextView.setText("Monthly Sports Rankings");
+                            isMonthlyRank = true;
 //                            updateRecyclerViewData("monthly");
+                            // Fetch target and progress data from firestore
+                            fetchTargetData();
                             break;
                     }
                 }
             }
         });
+
+        // Fetch target and progress data from firestore
+        fetchTargetData();
         return view;
     }
 
@@ -152,14 +167,14 @@ public class RankingFragment extends Fragment {
         final Dialog dialog = new Dialog(requireContext());
         dialog.setContentView(R.layout.dialog_set_target); // 使用自定义布局 dialog_time_picker.xml
 
-        // 获取 NumberPicker 和按钮
+        // Get NumberPicker and button
         final NumberPicker targetPicker = dialog.findViewById(R.id.targetPicker);
         Button btnSet = dialog.findViewById(R.id.btn_set);
         Button btnCancel = dialog.findViewById(R.id.btn_cancel);
 
-        // 设置 NumberPicker 的当前值
+        // Set picked num in NumberPicker
         targetPicker.setMinValue(1);
-        targetPicker.setMaxValue(12); // 设置 1 到 12 小时
+        targetPicker.setMaxValue(12);
 
         // Handle possible null or invalid targetHours
         int targetValue = 1;  // Default value
@@ -184,14 +199,15 @@ public class RankingFragment extends Fragment {
             }
         });
 
+        // close dialog when click cancel
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.dismiss();  // 取消，关闭对话框
+                dialog.dismiss();
             }
         });
 
-        dialog.show();  // 显示对话框
+        dialog.show();
     }
 
     /**
@@ -199,7 +215,7 @@ public class RankingFragment extends Fragment {
      */
     private void updateTargetDisplay(String targetHours) {
         if (targetHours.equals("--")) {
-            dailyTargetTextView.setText("-- hours");  // 如果没有目标，显示 "-- hours"
+            dailyTargetTextView.setText("-- hours");
         } else {
             dailyTargetTextView.setText(targetHours + " " + (targetHours.equals("1") ? "hour" : "hours"));
         }
@@ -217,29 +233,54 @@ public class RankingFragment extends Fragment {
     }
 
     /**
+     * Displays the updated the username and active time on the row above rank list.
+     */
+    private void updateTopRowDisplayDisplay(String userName, String totalActiveTime) {
+        if (totalActiveTime != null) {
+            topActiveTimeTextView.setText(totalActiveTime + " " + (totalActiveTime.equals("1") ? "hour" : "hours"));
+        } else {
+            topActiveTimeTextView.setText("0 hours");
+        }
+        topNameTextView.setText(userName);
+    }
+
+
+    /**
      * Fetch the target from firestore.
      */
     private void fetchTargetData() {
         if (userId != null) {
             DocumentReference userRef = db.collection("users").document(userId);
 //            userRef.get().addOnSuccessListener(documentSnapshot -> {
-            // 使用 addSnapshotListener 来监听数据库的变动
+            // using addSnapshotListener to listen the data change in database
             userRef.addSnapshotListener((documentSnapshot, error) -> {
                 if (error != null) {
                     Log.w("RankingFragment", "Listen failed.", error);
                     return;
                 }
                 if (documentSnapshot.exists()) {
-                    // Update daily active time UI
+                    Object target = documentSnapshot.get("target");
+                    Object fullName = documentSnapshot.get("fullName");
+
                     Map<String, Long> dailyActiveMap = (Map<String, Long>) documentSnapshot.get("dailyActive");
+                    Map<String, Long> monthlyActiveMap = (Map<String, Long>) documentSnapshot.get("monthlyActive");
+
                     // Get current date
-                    String today = getCurrentDate();
-                    // init dailyActiveTime to 0
+                    String currentDate = getCurrentDate();
+                    String currentMonth = getCurrentMonth();
+
+                    // init active time in hours to 0
                     double dailyActiveInHours = 0;
+                    double monthlyActiveInHours = 0;
+
                     // get today active time and convert to hours
-                    if (dailyActiveMap != null && dailyActiveMap.containsKey(today)) {
-                        long dailyActiveInSeconds = dailyActiveMap.get(today);
+                    if (dailyActiveMap != null && dailyActiveMap.containsKey(currentDate)
+                            && monthlyActiveMap != null && monthlyActiveMap.containsKey(currentMonth)) {
+                        long dailyActiveInSeconds = dailyActiveMap.get(currentDate);
                         dailyActiveInHours = convertSecondsToHours(dailyActiveInSeconds);
+                        long monthlyActiveInSeconds = monthlyActiveMap.get(currentMonth);
+                        monthlyActiveInHours = convertSecondsToHours(monthlyActiveInSeconds);
+
                     }
 //                    dailyActiveTime = Double.toString(dailyActiveInHours);
                     // convert to specific format
@@ -250,10 +291,10 @@ public class RankingFragment extends Fragment {
                         // if decimal
                         dailyActiveTime = String.format(Locale.getDefault(), "%.1f", dailyActiveInHours);
                     }
+                    // Update daily active time UI
                     updateDailyActiveDisplay(dailyActiveTime);
 
                     // Update target hours UI
-                    Object target = documentSnapshot.get("target");
                     if (target != null) {
                         targetHours = target.toString();
                     } else {
@@ -267,6 +308,37 @@ public class RankingFragment extends Fragment {
                         int progress = (int) ((dailyActiveInHours / targetInHours) * 100);  // Calculate progress percentage
                         progressBar.setProgress(progress);  // Update progress bar
                     }
+
+                    // update the row above rank list
+//                    // init dailyActiveTime to 0
+//                    double monthlyActiveInHours = 0;
+//                    // get today active time and convert to hours
+//                    if (monthlyActiveMap != null && monthlyActiveMap.containsKey(currentMonth)) {
+//                        long monthlyActiveInSeconds = monthlyActiveMap.get(currentMonth);
+//                        monthlyActiveInHours = convertSecondsToHours(monthlyActiveInSeconds);
+//                    }
+//                    dailyActiveTime = Double.toString(dailyActiveInHours);
+                    // convert to specific format
+                    if (monthlyActiveInHours == Math.floor(monthlyActiveInHours)) {
+                        // if int
+                        monthlyActiveTime = String.format(Locale.getDefault(), "%.0f", monthlyActiveInHours);
+                    } else {
+                        // if decimal
+                        monthlyActiveTime = String.format(Locale.getDefault(), "%.1f", monthlyActiveInHours);
+                    }
+
+                    if (fullName != null) {
+                        userName = fullName.toString();
+                    } else {
+                        userName = "--";  // Default value
+                    }
+
+                    if (isMonthlyRank){
+                        userTotalActiveTime = monthlyActiveTime;
+                    } else {
+                        userTotalActiveTime = dailyActiveTime;
+                    }
+                    updateTopRowDisplayDisplay(userName, userTotalActiveTime);
                 }
             });
 //                    .addOnFailureListener(e -> {
@@ -297,37 +369,29 @@ public class RankingFragment extends Fragment {
         }
     }
 
-//    /**
-//     * Fetch the target from firestore.
-//     */
-//    private void fetchDailyActiveHours() {
-//        if (userId != null) {
-//            DocumentReference userRef = db.collection("users").document(userId);
-//            userRef.get().addOnSuccessListener(documentSnapshot -> {
-//                if (documentSnapshot.exists()) {
-//                    Object target = documentSnapshot.get("target");
-//                    if (target != null) {
-//                        targetHours = target.toString();
-//                    } else {
-//                        targetHours = "--";  // Default value
-//                    }
-//                    updateTargetDisplay(targetHours);
-//                }
-//            }).addOnFailureListener(e -> {
-//                targetHours = "--";  // Set default value in case of failure
-//                updateTargetDisplay(targetHours);
-//            });
-//        }
-//    }
-    // 将秒转换为小时，并保留小数点后一位
+
+    /**
+     * Convert seconds to hours
+     */
     private double convertSecondsToHours(long seconds) {
-        return seconds / 3600.0; // 将秒转换为小时
+        return seconds / 3600.0;
     }
 
-    // 获取当前日期的函数，返回格式 "yyyy-MM-dd"
+    /**
+     * Method used to get current date in the format: YYYY-MM-DD
+     */
     private String getCurrentDate() {
         Date date = Calendar.getInstance().getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        return dateFormat.format(date); // YYYY-MM-DD 格式
+        return dateFormat.format(date);
+    }
+
+    /**
+     * Method used to get current date in the format: YYYY-MM-DD
+     */
+    private String getCurrentMonth() {
+        Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat monthFormat = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
+        return monthFormat.format(date);
     }
 }
